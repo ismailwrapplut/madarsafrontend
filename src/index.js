fe0c77f1ac17a6e0cd8cdd7b113c61c974afcf9e
Mohd Ismail <ismail.founder@gmail.com>
2024-05-12 00:13:39 +0530

    dd

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import { Route } from "react-router";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { withRouter } from "./utils"; // Import withRouter from your utility file
 
 import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
-import "./Login.css";
 import Dakhlaform from "./Dakhlaform";
+
+const LoginWithRouter = withRouter(Login); // Wrap Login component with withRouter HOC
+const RegisterWithRouter = withRouter(Register); // Wrap Register component with withRouter HOC
+const DashboardWithRouter = withRouter(Dashboard); // Wrap Dashboard component with withRouter HOC
+const DakhlaformWithRouter = withRouter(Dakhlaform); // Wrap Dakhlaform component with withRouter HOC
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/dakhlaform" element={<Dakhlaform />} />
-
+      <Route path="/" element={<LoginWithRouter />} />
+      <Route path="/register" element={<RegisterWithRouter />} />
+      <Route path="/dashboard" element={<DashboardWithRouter />} />
+      <Route path="/dakhlaform" element={<DakhlaformWithRouter />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
 );
-
