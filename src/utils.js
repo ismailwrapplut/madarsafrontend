02116339724505f80c8cb2f774e6550ea33762e6
Mohd Ismail <ismail.founder@gmail.com>
2024-05-12 00:08:36 +0530

    dd

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import React from "react";
 
 export const withRouter = (Component) => {
-  function Appwithrouter(){
+  function Appwithrouter(props){
     const navigate = useNavigate();
     return <Component {...props} navigate={navigate}/>;
   
