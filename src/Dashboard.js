5eadc135834208ace94d96d07e0b288431f9783c
Mohd Ismail <ismail.founder@gmail.com>
2024-05-12 15:30:39 +0530

    dd

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -650,26 +650,26 @@ class Dashboard extends Component {
           </div>
         </div>
         <hr />
-        <div style={{padding:"30%"}}>
+        <div style={{padding:"5% 20%"}}>
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src={one} alt="First slide"/>
+      <img class="d-block w-100" src={one} height="500px" alt="First slide"/>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src={two} alt="Second slide"/>
+      <img class="d-block w-100" src={two} height="500px" alt="Second slide"/>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src={three} alt="Fourth slide"/>
+      <img class="d-block w-100" src={three} height="500px" alt="Fourth slide"/>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src={four} alt="Fifth slide"/>
+      <img class="d-block w-100" src={four} height="500px" alt="Fifth slide"/>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src={five} alt="Sixth slide"/>
+      <img class="d-block w-100" src={five} height="500px" alt="Sixth slide"/>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src={six} alt="Seventh slide"/>
+      <img class="d-block w-100" src={six} height="500px" alt="Seventh slide"/>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
